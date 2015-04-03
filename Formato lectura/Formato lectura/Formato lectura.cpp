3f3582a8e536e57c22d973ad4385a94ca8d449b7
Bruno Aybar <miguel_bruno@hotmail.com>
2015-04-03 16:04:54 -0500

    test word

---
+++ Formato lectura/Formato lectura/Formato lectura.cpp
@@ -0,0 +1,35 @@
+// Formato lectura.cpp: archivo de proyecto principal.
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <vector>
+#include <functional>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(array<System::String ^> ^args)
+{
+	freopen("in.txt","rt",stdin);
+	freopen("out.txt","wt",stdout);
+
+	int nCasos;
+	scanf("%d",&nCasos);
+    for(int i=0;i<nCasos;i++){
+			int origen,destino;
+			scanf("%d %d",&origen,&destino);
+	}
+
+
+	//--------------------------------
+
+	freopen("in.txt","rt",stdin);
+	freopen("out.txt","wt",stdout);
+
+	int nCasos,nPuntos;
+	scanf("%d,%d",&nCasos,&nPuntos);
+    for(int i=0;i<nCasos;i++){
+			int origen,destino;
+			scanf("%d %d",&origen,&destino);
+	}
+}
